f4b764e903a2d81ee3bb3dda556884a9d271d16c
dcWORKS-chen <z.chen@dcworks.jp>
2025-05-26 13:04:30 +0900

    LikesRepositoryをPostLikesRepositoryに変更

--- eg_sns/src/main/java/com/example/eg_sns/repository/PostLikesRepository.java
+++ eg_sns/src/main/java/com/example/eg_sns/repository/PostLikesRepository.java
@@ -3,37 +3,38 @@ package com.example.eg_sns.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.example.eg_sns.entity.Likes;
+import com.example.eg_sns.entity.PostLikes;
 
 /**
  * いいね関連リポジトリインターフェース。
  *
  * @author chink
  */
-public interface LikesRepository extends PagingAndSortingRepository<Likes, Long>, CrudRepository<Likes, Long> {
-
+public interface PostLikesRepository extends PagingAndSortingRepository<PostLikes, Long>, CrudRepository<PostLikes, Long> {
 	/**
 	 * 指定されたユーザーIDと投稿IDに対応するLikeが存在するかを確認します。
 	 *
 	 * @param usersId ユーザーID
-	 * @param topicsId 投稿ID
+	 * @param postsId 投稿ID
 	 * @return Likeが存在すれば true、存在しなければ false
 	 */
-	boolean existsByUsersIdAndTopicsId(Long usersId, Long topicsId);
+	boolean existsByUsersIdAndPostsId(Long usersId, Long postsId);
 
 	/**
 	 * 指定されたユーザーIDと投稿IDに対応するLikeを削除します。
 	 *
 	 * @param usersId ユーザーID
-	 * @param topicsId 投稿ID
+	 * @param postsId 投稿ID
 	 */
-	void deleteByUsersIdAndTopicsId(Long usersId, Long topicsId);
+	void deleteByUsersIdAndPostsId(Long usersId, Long postsId);
 
 	/**
 	 * 指定された投稿に対するLike数をカウントします。
 	 *
-	 * @param topicsId 投稿ID
+	 * @param postsId 投稿ID
 	 * @return その投稿に対するLikeの数
 	 */
-	int countByTopicsId(Long topicsId);
+	int countByPostsId(Long postsId);
+
+
 }
